a2686550c1f7a47ba1833f7d17e2321c25b70a68
Etienne Lamy <etiennelamydlc@gmail.com>
2014-05-30 11:02:34 +0200

    add abide validation to forms

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,22 @@
+function toggleInputFieldsValidation(active){
+  $("#subjectField").attr("required", active);
+  $("#messageField").attr("required", active);
+}
+
 $(document).ready(function(){
 
   var active = $("#onButton").attr("checked");
   $("#subject-and-message").attr("hidden", !active);
+  toggleInputFieldsValidation(active);
 
   $("#offButton").click(function(){
     $("#subject-and-message").hide(500);
+    toggleInputFieldsValidation(false);
   });
 
   $("#onButton").click(function(){
     $("#subject-and-message").show(500);
+    toggleInputFieldsValidation(true);
   });
 
 });
